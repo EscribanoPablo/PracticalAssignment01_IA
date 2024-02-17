8528d226edfd54ecf6413b9cc5c36225c4b6c2bc
EscribanoPablo <pescribano@edu.tecnocampus.cat>
2024-02-17 04:50:26 +0100

    Sistema de pesca casi acabado

--- PracticalAssignment01_IA/Assets/PRACTICAS/Practica01/Scripts/PlayerController.cs
+++ PracticalAssignment01_IA/Assets/PRACTICAS/Practica01/Scripts/PlayerController.cs
@@ -13,19 +13,25 @@ public class PlayerController : MonoBehaviour
     public KeyCode leftKey = KeyCode.LeftArrow;
 
     public KeyCode ThrowFishingRodKey = KeyCode.Mouse0;
-    public KeyCode CollectFishingRodKey = KeyCode.Mouse0;
-
+    public KeyCode CollectFishingRodKey = KeyCode.Mouse1;
+    public LineRenderer fishingLine; 
 
     public GameObject fishHookPrefab;
+    public Transform startLinePosition;
+    //public float maxLengthFishingRod = 200;
+    //private float currentLength;
 
     private bool hasFish = false;
     private bool fishRodThrown; 
     private Camera camera;
+    private bool playerMovementEnabled;
+    private Vector3 mousePosition;
 
     void Start()
     {
         camera = Camera.main;
         fishRodThrown = false;
+        playerMovementEnabled = true;
     }
 
     void Update()
@@ -49,52 +55,83 @@ public class PlayerController : MonoBehaviour
         {
             CollectFishRod();
         }
+
     }
 
     private void ThrowFishingRoad()
     {
+        playerMovementEnabled = false;
+        Debug.Log("Cebo Lanzado");
+        //gameObject.SetActive(true);
         fishRodThrown = true;
-        Vector3 mousePosition = camera.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition = camera.ScreenToWorldPoint(Input.mousePosition);
         mousePosition.z = 0;
+
         GameObject fishHook = Instantiate(fishHookPrefab);
         fishHook.transform.position = mousePosition;
+
+        ShowFishingLine();
     }
 
     private void CollectFishRod()
     {
+        Debug.Log("Cebo Recogido");
+        playerMovementEnabled = true; 
         fishRodThrown = false;
-        //...
+        fishingLine.enabled = false;
+        //Destroy or hide Cebo
+
+        //if (fish.parent == fishHook)
+        // points++;
+        // Destroy(fish.gameObject);
+
+        //gameObject.SetActive(false);
+    }
+
+    private void ShowFishingLine()
+    {
+        fishingLine.startWidth = 2;
+        fishingLine.endWidth = 2;
+        fishingLine.startColor = Color.black;
+        fishingLine.endColor = Color.black;
+        fishingLine.sortingOrder = 5; 
+        fishingLine.SetPosition(0, startLinePosition.position);
+        fishingLine.SetPosition(1, mousePosition);
+        fishingLine.enabled = true;
     }
 
     private void PlayerMovement()
     {
-        Vector3 moveDirection = Vector3.zero;
+        if (playerMovementEnabled)
+        {
+            Vector3 moveDirection = Vector3.zero;
 
-        if (Input.GetKey(leftKey))
-        {
-            moveDirection += Vector3.left;
-        }
-        if (Input.GetKey(rightKey))
-        {
-            moveDirection += Vector3.right;
-        }
-        if (Input.GetKey(upKey))
-        {
-            moveDirection += Vector3.up;
-        }
-        if (Input.GetKey(downKey))
-        {
-            moveDirection += Vector3.down;
-        }
+            if (Input.GetKey(leftKey))
+            {
+                moveDirection += Vector3.left;
+            }
+            if (Input.GetKey(rightKey))
+            {
+                moveDirection += Vector3.right;
+            }
+            if (Input.GetKey(upKey))
+            {
+                moveDirection += Vector3.up;
+            }
+            if (Input.GetKey(downKey))
+            {
+                moveDirection += Vector3.down;
+            }
 
-        if (moveDirection != Vector3.zero)
-        {
-            moveDirection.Normalize();
+            if (moveDirection != Vector3.zero)
+            {
+                moveDirection.Normalize();
 
-            transform.position += moveDirection * speed * Time.deltaTime;
+                transform.position += moveDirection * speed * Time.deltaTime;
 
-            Quaternion targetRotation = Quaternion.LookRotation(Vector3.forward, moveDirection);
-            transform.rotation = targetRotation;
+                Quaternion targetRotation = Quaternion.LookRotation(Vector3.forward, moveDirection);
+                transform.rotation = targetRotation;
+            }
         }
     }
 }
